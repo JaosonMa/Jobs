52f9b3465713fa77e14b165a32e46a41ae993505
qqm <809631003@qq.com>
2021-02-03 14:03:21 +0800

    :tada: win 安装结果

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Jobs
 各种项目代码暂存地址
+
+
+[https://nchc.dl.sourceforge.net/project/zbar/zbar/0.10/zbar-0.10-setup.exe](https://nchc.dl.sourceforge.net/project/zbar/zbar/0.10/zbar-0.10-setup.exe "zbar-0.10-setup.exe")
+
+zbar官方提供的windows-install.exe安装后文件目录结构
---
+++ ZBar/zbarvars.bat
@@ -0,0 +1,9 @@
+@rem  Add the ZBar installation directory to the path
+@rem  so programs may be run from the command prompt
+@set PATH=%PATH%;E:\product\ZBar\install-0203\ZBar\bin
+@cd /D E:\product\ZBar\install-0203\ZBar
+@echo For basic command instructions type:
+@echo     zbarcam --help
+@echo     zbarimg --help
+@echo Try running:
+@echo     zbarimg -d examples\barcode.png
